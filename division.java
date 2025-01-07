54dd2787f65933d003c6351c1be7c043b132f565
kapil namdeo <kapilnamdeo2003@gmail>
2025-01-07 12:18:54 +0530

    division program

---
+++ division.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class helloworld {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int div=0;
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+
+
+        div=x/y;
+        System.out.println("division : "+div);
+    }
+}
