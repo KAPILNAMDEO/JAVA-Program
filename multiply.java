9d76bb4eb76e8d4788c0003e2e82ac8fe8588a57
kapil namdeo <kapilnamdeo2003@gmail>
2025-01-07 12:12:10 +0530

    multiply program

---
+++ multiply.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class helloworld {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int mul=0;
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+
+
+        mul=x*y;
+        System.out.println("multiply : "+mul);
+    }
+}
