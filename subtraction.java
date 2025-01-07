f03581a84ab194d823171cb118d7470f5b8b6a08
kapil namdeo <kapilnamdeo2003@gmail>
2025-01-07 11:30:09 +0530

    subtraction program

---
+++ subtraction.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class helloworld {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int subtract=0;
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+
+
+        subtract=x-y;
+        System.out.println("subtract : "+subtract);
+    }
+}
